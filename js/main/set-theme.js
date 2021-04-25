d1bd69dfefeba4a7478e59a42a4057851ac7046b
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-25 03:06:34 -0300

    feat: add rgba color

--- js/main/set-theme.js
+++ js/main/set-theme.js
@@ -5,7 +5,9 @@
     "--darken-1": "#262626",
     "--darken-2": "#323232",
     "--darken-3": "#4E4E4E",
+    "--darken-4": "#6E6E6E",
     "--primary": "#0066FF",
+    "--primary-opacity-4": "rgba(0, 102, 255, 0.4)",
     "--text": "#FAFAFA",
   };
   function setTheme() {
