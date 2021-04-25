ddef5bc17c8958cfadd10b02b0571825c3e539fa
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-24 22:58:52 -0300

    feat: set colors in the html

--- js/main/set-theme.js
+++ js/main/set-theme.js
@@ -1,1 +1,19 @@
-console.log("set theme file");
+"use strict";
+{
+  const theme = {
+    "--background": "#1F1F1F",
+    "--darken-1": "#262626",
+    "--darken-2": "#323232",
+    "--darken-3": "#4E4E4E",
+    "--primary": "#0066FF",
+    "--text": "#FAFAFA",
+  };
+  function setTheme() {
+    const keys = Object.keys(theme);
+    keys.forEach((key) => {
+      document.body.style.setProperty(key, theme[key]);
+    });
+  }
+
+  setTheme();
+}
