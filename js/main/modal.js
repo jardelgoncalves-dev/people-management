e3fd5a1ae231eb91b0a99045d15e43f2add01f72
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-26 22:31:19 -0300

    feat: add modal js

---
+++ js/main/modal.js
@@ -0,0 +1,19 @@
+const modal = document.getElementById("modal");
+const modalClose = document.getElementById("modal-close");
+const modalOpen = document.getElementById("modal-open");
+
+if (modal && modalClose) {
+  modalClose.addEventListener("click", () => {
+    if (modal.classList.contains("--visible")) {
+      modal.classList.remove("--visible");
+    }
+  });
+}
+
+if (modal && modalOpen) {
+  modalOpen.addEventListener("click", () => {
+    if (!modal.classList.contains("--visible")) {
+      modal.classList.add("--visible");
+    }
+  });
+}
