86fe4262b095ebfc5f2ba1b3031cc9efcec43bf5
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-29 23:53:10 -0300

    chore: add use strict

--- js/main/modal.js
+++ js/main/modal.js
@@ -1,19 +1,23 @@
-const modal = document.getElementById("modal");
-const modalClose = document.getElementById("modal-close");
-const modalOpen = document.getElementById("modal-open");
+"use strict";
 
-if (modal && modalClose) {
-  modalClose.addEventListener("click", () => {
-    if (modal.classList.contains("--visible")) {
-      modal.classList.remove("--visible");
-    }
-  });
-}
+{
+  const modal = document.getElementById("modal");
+  const modalClose = document.getElementById("modal-close");
+  const modalOpen = document.getElementById("modal-open");
 
-if (modal && modalOpen) {
-  modalOpen.addEventListener("click", () => {
-    if (!modal.classList.contains("--visible")) {
-      modal.classList.add("--visible");
-    }
-  });
+  if (modal && modalClose) {
+    modalClose.addEventListener("click", () => {
+      if (modal.classList.contains("--visible")) {
+        modal.classList.remove("--visible");
+      }
+    });
+  }
+
+  if (modal && modalOpen) {
+    modalOpen.addEventListener("click", () => {
+      if (!modal.classList.contains("--visible")) {
+        modal.classList.add("--visible");
+      }
+    });
+  }
 }
