3cb0f88703614a6643ee35b9262edb6fa7b80af4
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-05-01 00:38:20 -0300

    feat: add danger color

--- js/main/set-theme.js
+++ js/main/set-theme.js
@@ -6,10 +6,13 @@
     "--darken-2": "#323232",
     "--darken-3": "#4E4E4E",
     "--darken-4": "#6E6E6E",
+    "--darken-5": "#CDCDCD",
     "--primary": "#0066FF",
     "--secondary": "#DE72EF",
     "--primary-opacity-4": "rgba(0, 102, 255, 0.4)",
+    "--primary-opacity-2": "rgba(0, 102, 255, 0.2)",
     "--primary-opacity-1": "rgba(0, 102, 255, 0.1)",
+    "--danger": "#e84a4a",
     "--text": "#FAFAFA",
   };
   function setTheme() {
