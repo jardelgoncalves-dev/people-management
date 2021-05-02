c8b7c3546c974530804291b3a6fd4ae778790a24
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-05-01 23:30:03 -0300

    feat: add mount avatar select options by api

---
+++ js/main/fetch-avatar-options.js
@@ -0,0 +1,36 @@
+{
+  window.addEventListener("load", () => {
+    const list = document.querySelector("#select-avatar");
+    (() => {
+      fetch("http://localhost:3000/avatars")
+        .then((res) => res.json())
+        .then((data) => {
+          if (list) {
+            (data || []).forEach((item) => {
+              const buttonElement = document.createElement("button");
+              buttonElement.className = "select-avatar__item";
+              buttonElement.type = "button";
+              buttonElement.innerHTML = `
+                <img src="${URL_API}${item.image}" alt="Avatar" />
+              `;
+              list.appendChild(buttonElement);
+            });
+          }
+        })
+        .then(() => {
+          if (list) {
+            const buttons = list.querySelectorAll("button");
+            buttons.forEach((btn) => {
+              btn.addEventListener("click", () => {
+                buttons.forEach((unselectButton) =>
+                  unselectButton.classList.remove("--active")
+                );
+                btn.classList.toggle("--active");
+              });
+            });
+          }
+        })
+        .catch((error) => console.log(error));
+    })();
+  });
+}
